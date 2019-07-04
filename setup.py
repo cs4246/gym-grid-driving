8c9ef5de1a52142ed7dc4c7e5abba99b165842e7
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2019-07-04 14:44:33 +0800

    Use find_packages()

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_grid_driving',
       version='0.0.1',
-      install_requires=['gym', 'numpy']
+      install_requires=['gym', 'numpy'],
+      packages=find_packages(),
 )
