bb13f5f604e610b179a569d2f243490ea1257bea
Nishita <nishnikdutta@gmail.com>
2023-04-05 00:00:46 +0800

    Updated gym version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='gym_grid_driving',
       version='0.0.1',
-      install_requires=['gym==0.19', 'numpy'],
+      install_requires=['gym==0.22', 'numpy'],
       packages=find_packages(),
 )
